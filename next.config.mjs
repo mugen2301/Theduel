69e8a5309925f9a4bb1127cd22b270f5ef564a74
mugen2301 <creationsmugen@gmail.com>
2026-05-11 18:08:08 +0530

    fix customizer canvas build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      canvas: false
+    };
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
